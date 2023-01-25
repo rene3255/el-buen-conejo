c9bdf7d449d1511ed85577bfe7785e35e8274e0b
21harley <john.llanes.1996@gmail.com>
2023-01-24 22:21:08 -0400

    add icon

--- ebc_config/settings/development.py
+++ ebc_config/settings/development.py
@@ -9,7 +9,7 @@ import cloudinary.api
 env = environ.Env()
 environ.Env.read_env()
 
-DEBUG = env.bool('DEBUGG', default=False)
+DEBUG = env.bool('DEBUGG', default=True)
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-j)b73=p32@!h4asz^nt0aad&b5kybvg1dmwiqj&lsk4_l4bay-ebc'
