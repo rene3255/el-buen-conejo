ad74a9bc50b1117429e038609717dee167c04149
Rene Silva <rene.scre@gmail.com>
2023-01-24 18:02:52 -0600

    some fixes

--- ebc_config/settings/production.py
+++ ebc_config/settings/production.py
@@ -54,13 +54,6 @@ WSGI_APPLICATION = 'ebc_config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
