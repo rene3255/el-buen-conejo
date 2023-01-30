0abd7d18c136e541890f23109157898fa7279dd6
21harley <john.llanes.1996@gmail.com>
2023-01-30 13:05:50 -0400

    add template-home

--- homepage/urls.py
+++ homepage/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('home', views.home, name='home'),
+    path('', views.homepage, name='homepage'),
+    path('homepage', views.homepage, name='homepage'),
     path('login', views.login, name='login'),
-    path('register', views.register, name='register')
+    path('register', views.register, name='register'),
+    path('home', views.home, name='home')
 ]
--- homepage/views.py
+++ homepage/views.py
@@ -1,15 +1,28 @@
 from django.shortcuts import render
 
 # Create your views here.
-def home(request):
-    context = {'elbuenconejo': 'Hello from hompage view'}
+def homepage(request):
+    context = {
+        'elbuenconejo': 'Hello from hompage view'
+    }
     return render(request,'homepage/homepage.html',context)    
 
 def login(request):
-    context = {'elbuenconejo': 'Hello from hompage view'}
+    context = {
+        'elbuenconejo': 'Hello from hompage view'
+    }
     return render(request,'Login/Login.html',context)
 
 def register(request):
-    context = {'elbuenconejo': 'Hello from hompage view'}
-    return render(request,'Register/Register.html',context)    
+    context = {
+        'elbuenconejo': 'Hello from hompage view'
+    }
+    return render(request,'Register/Register.html',context) 
+
+def home(request):
+    context = {
+        'elbuenconejo': 'Hello from hompage view',
+        'path_rute':'home'
+    }
+    return render(request,'home/home.html',context)     
   
