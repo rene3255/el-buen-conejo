fe62f39fff3f7a8b19da6f2111ea353e23204b9b
21harley <john.llanes.1996@gmail.com>
2023-01-31 13:48:51 -0400

    add fix-navbar

--- static/js/details.js
+++ static/js/details.js
@@ -1,18 +1,12 @@
+import {menu_display} from "./navbar/navbar.js";
+
 const d=document;
 /*DOMContentLoaded es la primera carga del documento
 mas rapido que load */
 
 d.addEventListener('DOMContentLoaded',(e)=>{
-    const menu=document.querySelector(".menu-mobile__icon");
-    menu.addEventListener("click",(e)=>{
-        const menu_ul=document.querySelector(".menu");
-        if(menu.classList.contains('menu-mobile__icon-clouse')){
-            menu.classList.remove('menu-mobile__icon-clouse');
-            menu_ul.classList.remove('menu_active');
-        }else{
-            menu.classList.add('menu-mobile__icon-clouse');
-            menu_ul.classList.add('menu_active');
-
-        }
-    })
+    /*menu-navbar */
+    menu_display("menu-mobile__icon","menu","menu-mobile__icon-clouse","menu_active");
+    menu_display("input-user-setting","menu-user","input-user-setting-active","menu-user-active");
+    
 })
