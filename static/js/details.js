70e6902b368735a6f4f9698b58499117b6a4e9ae
21harley <john.llanes.1996@gmail.com>
2023-02-10 07:16:12 -0400

    add detail

---
+++ static/js/details.js
@@ -0,0 +1,9 @@
+import {menu_display} from './navbar/navbar.js'
+
+const d=document;
+/*DOMContentLoaded es la primera carga del documento
+mas rapido que load */
+
+d.addEventListener('DOMContentLoaded',(e)=>{
+
+})
