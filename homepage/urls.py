02ba153c142da57c7908d716435370e74939e8df
Rene Silva <rene.scre@gmail.com>
2024-08-05 19:40:42 -0600

    html anchor to home and homepage

--- homepage/urls.py
+++ homepage/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
+
 urlpatterns = [
-    path('', views.homepage, name='homepage'),
-    path('home', views.home, name='home'),
-    #path('homepage', views.homepage, name='homepage'),
-    path('makeDetails', views.details, name='makeDetails'),
-    path('profile', views.profile, name='profile'),
-    path('myprofile', views.myprofile, name='myprofile'),
+    path("", views.homepage, name="homepage"),
+    path("home", views.home, name="home"),
+    # path('homepage', views.homepage, name='homepage'),
+    path("makeDetails", views.details, name="makeDetails"),
+    path("profile", views.profile, name="profile"),
+    path("myprofile", views.myprofile, name="myprofile"),
 ]
